14774f20fafb5839c1dbfdc3dc38e188d930688a
Dany Leguy <danyleguy@Host-012.lan>
2026-02-22 00:04:56 +0100

    feat(theme): Theme setup

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,1 +1,20 @@
-/home/dany/.config/omarchy/current/theme/neovim.lua
+-- Return a list of plugins to load
+return {
+
+  -- Tokyonight colorscheme
+  {
+    "folke/tokyonight.nvim", -- plugin repo
+    lazy = false, -- load immediately
+    priority = 1000, -- load before everything else
+
+    config = function()
+      -- Set the theme style
+      require("tokyonight").setup({
+        style = "moon", -- night / storm / moon / day
+      })
+
+      -- Apply the colorscheme
+      vim.cmd("colorscheme tokyonight")
+    end,
+  },
+}
